7c3e2b83e014e8e291d5ef921ac55ffe342af885
thurmand <thu14002@byui.edu>
2015-10-09 16:59:18 -0600

    it didn't merge right

--- hello.java
+++ hello.java
@@ -1,10 +1,6 @@
 public class hello{
-	public static void main(String[] args){
-<<<<<<< HEAD
+	public static void main(String[] args)
 		System.out.println("Hello GitHub");
-=======
-		System.out.println("Hello GitHub World!");
 		System.out.println("Testing...");
->>>>>>> test
 	}
 }
